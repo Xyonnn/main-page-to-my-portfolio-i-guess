2e6e356f20cefed3c921273d4546adc2821d0152
Xyon <pj.kostera@gmail.com>
2026-08-26 12:34:57 +0200

    forgot to add things taht i changed... silly me

---
+++ app/components/footerComponents/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <div className="mt-auto w-full bg-blue-900 py-4 text-center text-white-400">
+            <p>Page made by me <span className="text-blue-400 underline"><a href="https://github.com/Xyonnn">Xyon</a></span></p>
+        </div>
+    );
+}
